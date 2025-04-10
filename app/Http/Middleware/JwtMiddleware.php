7c209661b232e06178a34dee55ae963453a00ac9
MBL It Manager <mblitimanager@gmail.com>
2025-04-10 16:12:20 +0300

    Refactor JWT middleware to streamline token exemption logic and improve readability

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -10,33 +10,23 @@ class JwtMiddleware
 {
     public function handle($request, Closure $next)
     {
+        // Routes exemptées de la vérification du token
         $except = [
             'api/login',
-            '/',
-            'api/user',
-            'api/register',
-            'api/docs',
-            'api',
-            'login',
-            'register',
-            'stagiaires/*',
-            'stagiaires',
-            'dashboard',
-            'logout',
             'api/logout',
+            'api',
+            'api/docs',
         ];
 
-        if (in_array($request->path(), $except)) {
-            return $next($request); // Bypass JWT check
-        }
-
-        try {
-            JWTAuth::parseToken()->authenticate();
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Token not valid'], 401);
+        // Vérifier si la route commence par /api/ et n'est pas dans les exceptions
+        if (str_starts_with($request->path(), 'api/') && !in_array($request->path(), $except)) {
+            try {
+                JWTAuth::parseToken()->authenticate();
+            } catch (\Exception $e) {
+                return response()->json(['error' => 'Token not valid'], 401);
+            }
         }
 
         return $next($request);
     }
-
 }
