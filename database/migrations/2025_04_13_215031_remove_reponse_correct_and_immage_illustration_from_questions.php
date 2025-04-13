a2b9150cae6cbf5e44134f07c53b874a79c0806e
mblitmanager <mblitmanager@gmail.com>
2025-04-13 21:55:09 +0300

    Remove 'reponse_correct' and 'immage_illustration' columns from 'questions' table in migration.

---
+++ database/migrations/2025_04_13_215031_remove_reponse_correct_and_immage_illustration_from_questions.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('reponse_correct');
+            $table->dropColumn('immage_illustration');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->string('reponse_correct')->nullable();
+            $table->string('immage_illustration')->nullable();
+        });
+    }
+}; 
