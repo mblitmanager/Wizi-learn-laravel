d3ccac1b40f329f311e2970689136763121efbde
Teddy <teddyrinah@gmail.com>
2025-06-05 13:10:38 +0300

    refactor template e-mail

--- app/Mail/NewQuizNotification.php
+++ app/Mail/NewQuizNotification.php
@@ -20,18 +20,11 @@ class NewQuizNotification extends Mailable
 
     public function build()
     {
-        // Pour le logo principal (petit), utiliser base64
-        $logoData = base64_encode(file_get_contents(public_path('assets/logo_wizi.png')));
-
-        // Pour la grande image, utiliser pièce jointe
         return $this->subject('Nouveau quiz disponible - ' . $this->quiz->titre)
             ->view('emails.new_quiz_notification', [
                 'quiz' => $this->quiz,
-                'logoData' => $logoData
-            ])
-            ->attach(public_path('assets/online.png'), [
-                'as' => 'online.png',
-                'mime' => 'image/png',
+                'logo' => public_path('assets/logo_wizi.png'),
+                'onlineImagePath' => public_path('assets/online.png')
             ]);
     }
 }
