a4fa9859c94549b7bb93473136b76e6212236e1e
mblitmanager <mblitmanage@gmail.com>
2025-10-09 08:18:21 +0300

    fix: Update card title headings from h3 to h5 for consistency across stagiaire views

--- resources/views/formateur/stagiaires/index.blade.php
+++ resources/views/formateur/stagiaires/index.blade.php
@@ -6,7 +6,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Mes Stagiaires</h3>
+                    <h5 class="card-title">Mes Stagiaires</h3>
                 </div>
                 <div class="card-body">
                     <!-- Statistiques -->
