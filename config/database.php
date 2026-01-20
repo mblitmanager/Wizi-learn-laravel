61030047c7aec8835a49c3d0221bfc983b93d9b0
MBL IT Manager <mblitmanager@gmail.com>
2026-01-20 09:18:39 +0300

    feat: Add default Laravel database configuration file.

--- config/database.php
+++ config/database.php
@@ -142,7 +142,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT') ?: 'predis',
+        'client' => in_array(env('REDIS_CLIENT'), ['phpredis', 'predis']) ? env('REDIS_CLIENT') : 'predis',
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
